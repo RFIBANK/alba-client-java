f9714990a96d34215982c7dcf5acf5b4ad92641f
Kutcher Sergey <daskain@gmail.com>
2019-09-23 18:18:36 +0300

    Upd стиль оформления аннотаций

--- src/main/java/ru/rficb/alba/TransferTypeBank.java
+++ src/main/java/ru/rficb/alba/TransferTypeBank.java
@@ -3,19 +3,35 @@ package ru.rficb.alba;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.experimental.NonFinal;
 
 @Builder
 @Getter
 public class TransferTypeBank {
 
-	@Builder.Default private static final TransferType transferType = TransferType.BANK;
-	@NonNull private String payerName;
-	@NonNull private String recipientName;
-	@NonNull private String recipientInn;
-	@NonNull private String recipientAccount;
-	@NonNull private String recipientBankName;
-	@NonNull private String recipientBankId;
-	@NonNull private String recipientBankCorrespondentAccount;
+	@Builder.Default 
+	private static final TransferType transferType = TransferType.BANK;
+	
+	@NonNull 
+	private String payerName;
+	
+	@NonNull 
+	private String recipientName;
+	
+	@NonNull 
+	private String recipientInn;
+	
+	@NonNull 
+	private String recipientAccount;
+	
+	@NonNull 
+	private String recipientBankName;
+	
+	@NonNull 
+	private String recipientBankId;
+	
+	@NonNull 
+	private String recipientBankCorrespondentAccount;
 	
 	public TransferType getTransferType() {
 	    return transferType;
--- src/main/java/ru/rficb/alba/TransferTypeBankGov.java
+++ src/main/java/ru/rficb/alba/TransferTypeBankGov.java
@@ -8,18 +8,41 @@ import lombok.NonNull;
 @Getter
 public class TransferTypeBankGov {
 
-	@Builder.Default private static final TransferType transferType = TransferType.BANK_GOV;
-	@NonNull private String payerType101;
-	@NonNull private String kpp103;
-	@NonNull private String kbk104;
-	@NonNull private String okato105;
-	@NonNull private String paymentReason106;
-	@NonNull private String taxPeriod107;
-	@NonNull private String taxDocNum108;
-	@NonNull private String taxDocDate109;
-	@NonNull private String paymentType110;
-	@NonNull private String kod22;
-	@NonNull private String empSystemId;
+	@Builder.Default 
+	private static final TransferType transferType = TransferType.BANK_GOV;
+	
+	@NonNull 
+	private String payerType101;
+	
+	@NonNull 
+	private String kpp103;
+	
+	@NonNull 
+	private String kbk104;
+	
+	@NonNull 
+	private String okato105;
+	
+	@NonNull 
+	private String paymentReason106;
+	
+	@NonNull 
+	private String taxPeriod107;
+	
+	@NonNull 
+	private String taxDocNum108;
+	
+	@NonNull 
+	private String taxDocDate109;
+	
+	@NonNull 
+	private String paymentType110;
+	
+	@NonNull 
+	private String kod22;
+	
+	@NonNull 
+	private String empSystemId;
 	
 	public TransferType getTransferType() {
 	    return transferType;
